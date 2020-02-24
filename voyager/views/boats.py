29224a72e086519b2087e82858a9ada8ffa4a4f7
Tanjin Prity <37024552+tanjinprity4@users.noreply.github.com>
2020-02-23 22:54:44 -0800

    List the boats by order of popularity (number of voyages) from most popular to least

--- voyager/views/boats.py
+++ voyager/views/boats.py
@@ -13,6 +13,9 @@ def boats(conn):
 def get_all_boats_from_sailor_name(conn, s_name):
     return execute(conn, "SELECT DISTINCT b.bid, b.name, b.color FROM ((Sailors AS s INNER JOIN Voyages As v ON s.sid = v.sid) INNER JOIN Boats AS b ON v.bid = b.bid) WHERE s.name = :s_name", {'s_name': s_name})
 
+def get_all_boats_by_popularity(conn):
+    return execute(conn, "SELECT b.bid, b.name, b.color, count(v.bid) AS number_of_voyages FROM (Voyages As v INNER JOIN Boats AS b ON v.bid = b.bid) GROUP BY v.bid ORDER BY count(v.bid) desc")
+
 def views(bp):
     @bp.route("/boats")
     def _boats():
@@ -26,3 +29,9 @@ def views(bp):
             sailors_name = request.args.get('sailor-name')
             rows = get_all_boats_from_sailor_name(conn, sailors_name)
         return render_template("table.html", name="Boats sailed by %s" %sailors_name, rows=rows)
+
+    @bp.route("/boats/by-popularity")
+    def _get_all_boats_by_popularity():
+        with get_db() as conn:
+            rows = get_all_boats_by_popularity(conn)
+        return render_template("table.html", name="List of boats by order of popularity", rows=rows)
